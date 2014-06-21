caef21bad0d38e73f35674ed3bdc592dd9a99e10
Lena <lena.ohuryayeva@gmail.com>
2014-06-21 21:03:45 +0300

    add backend

---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+var mime = require('mime');
+
+http.createServer(function(req, res){
+    var couchUrl = "epam-tasks-app.iriscouch.com";
+
+    if (req.url.lastIndexOf("/couch", 0) === 0){// check if url starts with '/couch'
+        var headers = req.headers;
+        headers.host = couchUrl;
+
+        delete headers.authorization;// temporary
+
+        var options = {
+            hostname: couchUrl,
+            port: req.port,
+            path: req.url.replace("/couch", ""),
+            method: req.method,
+            headers:headers
+        };
+         http.request(options,function(response){
+            response.pipe(res);
+
+        }).end();
+    } else {
+        var file = req.url === '/' ? 'index.html' : req.url.substr(1);
+        fs.exists(file, function(exists){
+            if (exists){
+                var stat = fs.statSync(file);
+
+                res.writeHead(200, {
+                    'Content-Type': mime.lookup(file),
+                    'Content-Length': stat.size
+                });
+
+                fs.createReadStream(file).pipe(res);
+            } else {
+                res.statusCode = 404;
+                res.end();
+            }
+        });
+    }
+}).listen(80);
